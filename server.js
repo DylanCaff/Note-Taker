e16ad9ccf9c0562136b77a84a132ce956f05b8f7
DylanCaff <dylancaff0902@gmail.com>
2023-03-16 23:52:30 -0400

    made createNewNote function

--- server.js
+++ server.js
@@ -21,7 +21,24 @@ app.get('/', (req, res) => {
 });
 
 app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
+function createNewNote(body, notesArray) {
+    const newNote = body;
+    if (!Array.isArray(notesArray))
+        notesArray = [];
+    
+    if (notesArray.length === 0)
+        notesArray.push(0);
 
+    body.id = notesArray[0];
+    notesArray[0]++;
+
+    notesArray.push(newNote);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify(notesArray, null, 2)
+    );
+    return newNote;
+}

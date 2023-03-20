ba6545b6cdb6887e9df5d8d10ea7dee1b133f18e
DylanCaff <dylancaff0902@gmail.com>
2023-03-20 10:34:27 -0400

    v2

--- server.js
+++ server.js
@@ -22,63 +22,63 @@ app.get('/api/notes', (req, res) => {
     res.json(allNotes.slice(1));
 });
 
-// app.route("/api/notes")
-//     .post(function (req, res) {
-        // let jsonFilePath = path.join(__dirname, "./db/db.json");
-        // let newNote = req.body;
-        // let highestId = 99;
+app.route("/api/notes")
+    .post(function (req, res) {
+        let jsonFilePath = path.join(__dirname, "./db/db.json");
+        let newNote = req.body;
+        let highestId = 99;
 
-        // for (let i = 0; i < database.length; i++) {
-        //     let individualNote = database[i];
+        for (let i = 0; i < database.length; i++) {
+            let individualNote = database[i];
 
-        //     if (individualNote.id > highestId) {
+            if (individualNote.id > highestId) {
 
-        //         highestId = individualNote.id;
-        //     }
-        // }
-        // newNote.id = highestId + 1;
-        // database.push(newNote)
+                highestId = individualNote.id;
+            }
+        }
+        newNote.id = highestId + 1;
+        database.push(newNote)
 
-        // fs.writeFile(jsonFilePath, JSON.stringify(database), function (err) {
+        fs.writeFile(jsonFilePath, JSON.stringify(database), function (err) {
 
-        //     if (err) {
-        //         return console.log(err);
-        //     }
-        //     console.log("Your note was saved!");
-        // });
-        // res.json(newNote);
-    // });
+            if (err) {
+                return console.log(err);
+            }
+            console.log("Your note was saved!");
+        });
+        res.json(newNote);
+    });
 
 
-// app.post('/api/notes', (req, res) => {
-//     const newNote = createNewNote(req.body, allNotes);
-//     res.json(newNote);
-// });
+app.post('/api/notes', (req, res) => {
+    const newNote = createNewNote(req.body, allNotes);
+    res.json(newNote);
+});
 
-// const createNewNote = (body, noteArr) => {
-//     let jsonFilePath = path.join(__dirname, "./db/db.json");
-//     let newNote = body;
-//     let highestId = 99;
+const createNewNote = (body, noteArr) => {
+    let jsonFilePath = path.join(__dirname, "./db/db.json");
+    let newNote = body;
+    let highestId = 99;
 
-//     for (let i = 0; i < noteArr.length; i++) {
-//         let individualNote = database[i];
-//         if (individualNote.id > highestId) {
+    for (let i = 0; i < noteArr.length; i++) {
+        let individualNote = database[i];
+        if (individualNote.id > highestId) {
 
-//             highestId = individualNote.id;
-//         }
-//     }
-//     newNote.id = highestId + 1;
-//     noteArr.push(newNote)
+            highestId = individualNote.id;
+        }
+    }
+    newNote.id = highestId + 1;
+    noteArr.push(newNote)
 
-//     fs.writeFile(jsonFilePath, JSON.stringify(noteArr), function (err) {
+    fs.writeFile(jsonFilePath, JSON.stringify(noteArr), function (err) {
 
-//         if (err) {
-//             return console.log(err);
-//         }
-//         console.log("Your note was saved!");
-//     });
-//     res.json(newNote);
-// }
+        if (err) {
+            return console.log(err);
+        }
+        console.log("Your note was saved!");
+    });
+    res.json(newNote);
+}
 
 app.listen(PORT, function () {
     console.log("App listening on PORT http://localhost:" + PORT);

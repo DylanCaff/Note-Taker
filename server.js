2f9eab74327b22063ae99ddb0db5ac8dccaf11a1
DylanCaff <dylancaff0902@gmail.com>
2023-03-20 10:32:51 -0400

    v1

--- server.js
+++ server.js
@@ -1,67 +1,85 @@
-const fs = require('fs');
-const path = require('path');
 const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const allNotes = require('./db/db.json');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
-
-const allNotes = require('./public/db/db.json');
-
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-
-app.get('/api/notes', (req, res) => {
-    res.json(allNotes.slice(1));
-});
-
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+    res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
 app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
-app.route("/api/notes")
-    .get(function (req, res) {
-        res.json(database);
-    })
-    .post(function (req, res) {
-        let jsonFilePath = path.join(__dirname, "/db/db.json");
-        let newNote = req.body;
-        let highestId = 99;
-
-        for (let i = 0; i < database.length; i++) {
-            let individualNote = database[i];
-
-            if (individualNote.id > highestId) {
-
-                highestId = individualNote.id;
-            }
-        }
-        newNote.id = highestId + 1;
-        database.push(newNote)
-
-        fs.writeFile(jsonFilePath, JSON.stringify(database), function (err) {
-
-            if (err) {
-                return console.log(err);
-            }
-            console.log("Your note was saved!");
-        });
-        res.json(newNote);
-    });
-
-
-app.post('/api/notes', (req, res) => {
-    const newNote = createNewNote(req.body, allNotes);
-    res.json(newNote);
+app.get('/api/notes', (req, res) => {
+    res.json(allNotes.slice(1));
 });
 
+// app.route("/api/notes")
+//     .post(function (req, res) {
+        // let jsonFilePath = path.join(__dirname, "./db/db.json");
+        // let newNote = req.body;
+        // let highestId = 99;
+
+        // for (let i = 0; i < database.length; i++) {
+        //     let individualNote = database[i];
+
+        //     if (individualNote.id > highestId) {
+
+        //         highestId = individualNote.id;
+        //     }
+        // }
+        // newNote.id = highestId + 1;
+        // database.push(newNote)
+
+        // fs.writeFile(jsonFilePath, JSON.stringify(database), function (err) {
+
+        //     if (err) {
+        //         return console.log(err);
+        //     }
+        //     console.log("Your note was saved!");
+        // });
+        // res.json(newNote);
+    // });
+
+
+// app.post('/api/notes', (req, res) => {
+//     const newNote = createNewNote(req.body, allNotes);
+//     res.json(newNote);
+// });
+
+// const createNewNote = (body, noteArr) => {
+//     let jsonFilePath = path.join(__dirname, "./db/db.json");
+//     let newNote = body;
+//     let highestId = 99;
+
+//     for (let i = 0; i < noteArr.length; i++) {
+//         let individualNote = database[i];
+//         if (individualNote.id > highestId) {
+
+//             highestId = individualNote.id;
+//         }
+//     }
+//     newNote.id = highestId + 1;
+//     noteArr.push(newNote)
+
+//     fs.writeFile(jsonFilePath, JSON.stringify(noteArr), function (err) {
+
+//         if (err) {
+//             return console.log(err);
+//         }
+//         console.log("Your note was saved!");
+//     });
+//     res.json(newNote);
+// }
+
 app.listen(PORT, function () {
-    console.log("App listening on PORT " + PORT);
+    console.log("App listening on PORT http://localhost:" + PORT);
 });
